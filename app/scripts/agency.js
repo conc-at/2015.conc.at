5990232b8d4804662ba935aa2cad9c88896fdea5
Christoph Witzko <github@christophwitzko.com>
2014-11-24 23:12:58 +0100

    feat: inital sponsor drag and drop feature

--- app/scripts/agency.js
+++ app/scripts/agency.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 /*!
  * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
@@ -6,71 +6,96 @@
  */
 
 $(function() {
-  var hash = location.hash;
+  var hash = location.hash
   if (!/Modal$/.test(hash)) {
-    return;
+    return
   }
-  $(hash).modal('show');
-});
+  $(hash).modal('show')
+})
+
+/*global DragDrop:false */
+$(function(){
+  if(typeof window.FileReader === 'undefined') return
+
+  var $drag = $('.dragzone')
+  $drag.on('dragover', $drag.removeClass.bind($drag, 'fa-plus'))
+  $drag.on('dragleave ', $drag.addClass.bind($drag, 'fa-plus'))
+
+  new DragDrop($drag[0], function (files) {
+    var file = files.pop()
+    $drag.addClass('dragged')
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      $('#newsponsor')
+        .append($('<img>').attr({
+          src: event.target.result,
+          width: '200',
+          height: 'auto'
+        }))
+        .show()
+    }
+    reader.readAsDataURL(file);
+  })
+})
 
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
   $('a.page-scroll').bind('click', function(event) {
-    var href = $(this).attr('href');
+    var href = $(this).attr('href')
     $('html, body').stop().animate({
       scrollTop: $(href).offset().top
-    }, 1500, 'easeInOutExpo');
-    history.pushState({}, '', href);
-    event.preventDefault();
-  });
-});
+    }, 1500, 'easeInOutExpo')
+    history.pushState({}, '', href)
+    event.preventDefault()
+  })
+})
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
   target: '.navbar-fixed-top'
-});
+})
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-  $('.navbar-toggle:visible').click();
-});
+  $('.navbar-toggle:visible').click()
+})
 
-window.addEventListener('updateready', window.location.reload);
+window.addEventListener('updateready', window.location.reload)
 
 if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
-  window.location.reload();
+  window.location.reload()
 }
 
 // Map
 /*global google:false */
 $(function() {
-  var map;
+  var map
   if (!google) {
-    return;
+    return
   }
-  var $map = $('#map').show();
+  var $map = $('#map').show()
 
   var setScroll = function(option) {
     if (!map) {
-      return;
+      return
     }
-    map.setOptions({scrollwheel: option});
-  };
+    map.setOptions({scrollwheel: option})
+  }
 
   var initialize = function() {
     map = new google.maps.Map($map[0], {
       zoom: 11,
       scrollwheel: false,
       center: new google.maps.LatLng(47.761026, 13.069313)
-    });
+    })
 
-    var uni = 'University of Applied Sciences';
+    var uni = 'University of Applied Sciences'
     var marker = new google.maps.Marker({
       position: new google.maps.LatLng(47.723379, 13.087777),
       map: map,
       title: uni
-    });
+    })
 
     var infowindow = new google.maps.InfoWindow({
       content: '<h2 class="section-heading">Our amazing venue</h2>' +
@@ -80,30 +105,30 @@ $(function() {
             '<i class="fa fa-external-link"></i>' +
           '</a>' +
         '</h3>'
-    });
+    })
 
-    infowindow.open(map, marker);
+    infowindow.open(map, marker)
 
     google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(map, marker);
-    });
+      infowindow.open(map, marker)
+    })
 
     google.maps.event.addListener(map, 'mousedown', function() {
-      setScroll(true);
-    });
-  };
+      setScroll(true)
+    })
+  }
 
   $('body').on('mousedown', function(event) {
-    var insideMap = $(event.target).parents('#map').length > 0;
+    var insideMap = $(event.target).parents('#map').length > 0
 
     if(!insideMap) {
-      setScroll(false);
+      setScroll(false)
     }
-  });
+  })
 
   $(window).scroll(function() {
-    setScroll(false);
-  });
+    setScroll(false)
+  })
 
-  google.maps.event.addDomListener(window, 'load', initialize);
-});
+  google.maps.event.addDomListener(window, 'load', initialize)
+})
