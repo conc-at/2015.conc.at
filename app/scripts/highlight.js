4473f1d8d6ec607018e62da3fabcf1fee19c481b
Christoph Witzko <github@christophwitzko.com>
2015-02-22 01:31:07 +0100

    fix(highlight): time compare at getCurrentTalks

--- app/scripts/highlight.js
+++ app/scripts/highlight.js
@@ -3,22 +3,23 @@ $(function () {
   /* jshint camelcase: false */
   // 2015-03-07 for debugging
   var today = '2015-03-07' || (new Date()).toISOString().substr(0, 10)
-  var rooms = ['110', 'lounge', 'audimax', '017']
+  var rooms = ['room-110', 'lounge', 'audimax', 'room-017']
 
   function getCurrentTalks (data) {
     var nextTalks = {}
-    var now = new Date()
-    var compare = new Date(today)
+    var time = new Date()
+    var now = new Date(today)
     // 12:01 for debugging
-    compare.setHours(12 || now.getHours())
-    compare.setMinutes(1 || now.getMinutes())
+    now.setHours(12 || time.getHours())
+    now.setMinutes(1 || time.getMinutes())
     Object.keys(data).forEach(function (room) {
       data[room][today].some(function (v) {
-        var time = new Date(v.start_time)
-        if (((time - compare) / 1000 * 60 * 60) > 10) {
-          nextTalks[room] = v
-          return true
-        }
+        var diff = new Date(v.start_time) - now
+
+        if (diff < 0 || diff > 1000 * 60 * 60) return false
+
+        nextTalks[room] = v
+        return true
       })
     })
     return nextTalks
@@ -36,7 +37,7 @@ $(function () {
     Object.keys(currentTalks).forEach(function (room) {
       var el = $('#talks a[data-slug="' + currentTalks[room].slug + '"]')
       if (el.length > 0) {
-        el.addClass('current-' + room.toLowerCase())
+        el.addClass('current-' + room.toLowerCase().replace(' ', '-'))
       }
     })
     setTimeout(updateTalks.bind(null, currentTalks), 5000)
