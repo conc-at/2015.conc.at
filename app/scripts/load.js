00ef6b3673cacbe61dc61d7cb023e333c78eda1e
Hannes Moser <hannes@impossiblearts.com>
2017-04-19 23:20:49 +0200

    chore: fix linting issues

--- app/scripts/load.js
+++ app/scripts/load.js
@@ -1,12 +1,21 @@
 (function() {
   'use strict'
+
+  function destroy(waypoint) {
+    if (waypoint && (typeof waypoint.destroy === 'function')) {
+      waypoint.destroy()
+    }
+  }
+
   /* global Waypoint */
   /* eslint no-new: 0 */
   var tloaded = false
   new Waypoint({
     element: document.querySelector('#talks'),
     handler: function() {
-      if (tloaded) return destroy(this)
+      if (tloaded) {
+        return destroy(this)
+      }
       tloaded = true
       var script = document.createElement('script')
       script.src = 'https://js.tito.io/v1'
@@ -19,15 +28,15 @@
   new Waypoint({
     element: document.querySelector('#about'),
     handler: function() {
-      if (gloaded) return destroy(this)
+      if (gloaded) {
+        return destroy(this)
+      }
       gloaded = true
       window.__loadedMap = true
-      if (typeof window.__initializeMap === 'function') window.__initializeMap()
+      if (typeof window.__initializeMap === 'function') {
+        window.__initializeMap()
+      }
       destroy(this)
     }
   })
-
-  function destroy (waypoint) {
-    if (waypoint && (typeof waypoint.destroy === 'function')) waypoint.destroy()
-  }
 })()
